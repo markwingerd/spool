b5ca9b5a7118cf943524763b48cd837c3b03c979
Mark Wingerd <markwingerd@yahoo.com>
2012-11-10 18:15:22 -0800

    Added Tests and point system.

--- gmech.py
+++ gmech.py
@@ -2,7 +2,7 @@ import math
 
 class Gmech:
     """"""
-    def __init__(self, weekly_pool=300000, matches_per_hour=3, points_per_game=10000, inactivity_time=5, inactivity_drop=0.20):
+    def __init__(self, weekly_pool=300000, matches_per_hour=3, points_per_game=5000, inactivity_time=5, inactivity_drop=0.20):
         """"""
         self.weekly_pool = weekly_pool
         self.matches_per_hour = matches_per_hour
@@ -10,14 +10,14 @@ class Gmech:
         self.inactivity_time = inactivity_time
         self.inactivity_drop = inactivity_drop
 
-    def get_match_points(self, match_number):
+    def get_match_points(self, week_number, match_number):
         """"""
-        #output = round(self.points_per_game * 1/pow(match_number+1,0.33))
-        output = round(self.points_per_game * 1/(match_number+1))
+        output = 0
+        # The first week is a sample period.
+        if week_number is 0:
+            multiplier = float(100-match_number)/100
+            if multiplier < 0:
+                multiplier = 0
+            output = (self.points_per_game-1000)*multiplier +1000
 
-        #Points never fall below 1000
-        if output < 1000:
-            output = 1000
-
-        #print output
         return output
--- merc.py
+++ merc.py
@@ -9,14 +9,28 @@ class Merc:
         self.this_weeks_pool = 0
         self.pool = 0
         self.points = 0
+        self.week = 0
 
-    def week(self, hours=0):
+        self.assumed_weekly_hours = 0
+
+    def play(self, hours=0):
         """"""
         self.this_weeks_pool = self.gmech.weekly_pool
+
+        # Add skill points for each match.
+        for i in range(hours*self.gmech.matches_per_hour):
+            self.points += self.gmech.get_match_points(0, i)
+            self.this_weeks_pool -= self.gmech.get_match_points(self.week, i)
 
         # If the merc doesn't meet the minimum time, apply penalty on this_weeks_pool.
         if hours < self.gmech.inactivity_time:
             self.this_weeks_pool *= (1-self.gmech.inactivity_drop)
 
         # Week is over, add the remaining this_weeks_pool to pool
-        self.pool = self.this_weeks_pool
+        self.pool += self.this_weeks_pool
+        self._update_weekly_hours(hours)
+
+    def _update_weekly_hours(self, hours):
+        """"""
+        self.assumed_weekly_hours = (self.assumed_weekly_hours*self.week + hours)/(self.week+1)
+        self.week += 1
