5bcd1603bca2fcba5d06d116ba5cec8cf88c6314
Mark Wingerd <markwingerd@yahoo.com>
2012-11-11 22:01:05 -0800

    Added gmech class init.

--- gmech.py
+++ gmech.py
@@ -3,7 +3,7 @@ import math
 class Gmech:
     """"""
     def __init__(self, weekly_pool=300000, matches_per_hour=3, inactivity_time=5, inactivity_drop=0.20):
-        # Init for all mech variables.
+        """Init for all mech variables."""
         self.weekly_pool = weekly_pool
         self.matches_per_hour = matches_per_hour
         self.inactivity_time = inactivity_time
