ce17b9b47e651cad0c2ec0737019edf68edf1c92
Mark Wingerd <markwingerd@yahoo.com>
2012-11-11 22:15:35 -0800

    Added merc class init.

--- merc.py
+++ merc.py
@@ -2,7 +2,14 @@ import gmech
 
 class Merc:
     """"""
-    pass
-    # Initialize Merc variables
+    def __init__(self):
+        """Initialize Merc variables."""
+        self.points = 0
+        self.pool = 0
+        self.this_weeks_pool = 0
     # Method for matches per week.
     # Method for dealing with reduced points for inactivity.
+
+if __name__ == '__main__':
+    merc = Merc()
+    print merc.pool
--- tests.py
+++ tests.py
@@ -1,6 +1,7 @@
 import unittest
 
 from gmech import Gmech
+from merc import Merc
 
 class TestSpool(unittest.TestCase):
     """"""
@@ -11,6 +12,13 @@ class TestSpool(unittest.TestCase):
         self.assertEqual(gmech.matches_per_hour,gmech.matches_per_hour)
         self.assertEqual(gmech.inactivity_time,gmech.inactivity_time)
         self.assertEqual(gmech.inactivity_drop,gmech.inactivity_drop)
+
+    def test_merc_init(self):
+        """Tests the internal variables."""
+        merc = Merc()
+        self.assertEqual(merc.points,0)
+        self.assertEqual(merc.pool,0)
+        self.assertEqual(merc.this_weeks_pool,0)
 
 if __name__ == '__main__':
     unittest.main()
