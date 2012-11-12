b56dee81fa1d1b6e46cb9d9963e59b9e237043e0
Mark Wingerd <markwingerd@yahoo.com>
2012-11-11 21:45:24 -0800

    Added vague structure.

--- gmech.py
+++ gmech.py
@@ -3,3 +3,7 @@ import math
 class Gmech:
     """"""
     pass
+    # Init for all mech variables.
+    # Get weekly pool.
+    # Get match points based on match number and total pool.
+    # Get point reduction for low weekly hours.
--- merc.py
+++ merc.py
@@ -3,3 +3,6 @@ import gmech
 class Merc:
     """"""
     pass
+    # Initialize Merc variables
+    # Method for matches per week.
+    # Method for dealing with reduced points for inactivity.
