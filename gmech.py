1217b185e584c7f3023d272f68f48606174ef5f0
Mark Wingerd <markwingerd@yahoo.com>
2012-11-11 21:59:30 -0800

    Added gmech class init.

--- gmech.py
+++ gmech.py
@@ -2,8 +2,17 @@ import math
 
 class Gmech:
     """"""
-    pass
-    # Init for all mech variables.
+    def __init__(self, weekly_pool=300000, matches_per_hour=3, inactivity_time=5, inactivity_drop=0.20):
+        # Init for all mech variables.
+        self.weekly_pool = weekly_pool
+        self.matches_per_hour = matches_per_hour
+        self.inactivity_time = inactivity_time
+        self.inactivity_drop = inactivity_drop
     # Get weekly pool.
     # Get match points based on match number and total pool.
     # Get point reduction for low weekly hours.
+
+if __name__ == '__main__':
+    # Used for testing scenerios/flow
+    gmech = Gmech()
+    print gmech.weekly_pool
--- tests.py
+++ tests.py
@@ -1,8 +1,16 @@
 import unittest
 
+from gmech import Gmech
+
 class TestSpool(unittest.TestCase):
     """"""
-    pass
+    def test_gmech_init(self):
+        """Tests the Init arguments."""
+        gmech = Gmech()
+        self.assertEqual(gmech.weekly_pool,gmech.weekly_pool)
+        self.assertEqual(gmech.matches_per_hour,gmech.matches_per_hour)
+        self.assertEqual(gmech.inactivity_time,gmech.inactivity_time)
+        self.assertEqual(gmech.inactivity_drop,gmech.inactivity_drop)
 
 if __name__ == '__main__':
     unittest.main()
