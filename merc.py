bb3c26ceae4f0120e81ec5c61323ad44ead4efe4
Mark Wingerd <markwingerd@yahoo.com>
2012-11-18 21:40:07 -0800

    Refactored merc class.

--- merc.py
+++ merc.py
@@ -16,13 +16,14 @@ class Merc:
         # self.history must not be larger than gmech.weeks_in_history
         if len(self.history) > self.gmech.weeks_in_history:
             self.history.pop(0)
+        # If all of self.history is 0 then re-init self.history to [].
+        if sum(self.history) == 0:
+            self.history = []
 
     def get_history(self):
         """Manages merc history and returns the average hours."""
-        # If all of self.history is 0 then re-init self.history to [].
         # Return 0 to avoid Div by 0.
-        if sum(self.history) == 0:
-            self.history = []
+        if len(self.history) == 0:
             return 0
         # Find the average hours in history.
         return sum(self.history)/float(len(self.history))
@@ -49,12 +50,12 @@ class Merc:
                 average_points += match_points ###
             average_points /= matches_played ###
         # Finalize the week.
-        self.add_history(hours_played) # History needs to be updated AFTER the week.
+        self.add_history(hours_played)
         self.this_weeks_pool -= self.gmech.get_point_reduction(hours_played,self.this_weeks_pool)
         self.pool += self.this_weeks_pool
         print 'Matches: %3i - Points gained: %10.1f - Pool left: %10.1f - Average points: %7.1f' % (matches_played, self.points, self.pool, average_points)
 
-#NEGATIVE POOL GOING TO REDUCTION. FIX
+
 if __name__ == '__main__':
     gmech = Gmech(weekly_pool=500000, inactivity_time=5,inactivity_drop=0.40)
     print 'Addicted Merc'
