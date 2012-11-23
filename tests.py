b8b3e6e7fe80550a58cd48aa41ebe1da1d680cae
Mark Wingerd <markwingerd@yahoo.com>
2012-11-23 15:26:29 -0800

    Fixed a few tests.

--- tests.py
+++ tests.py
@@ -12,10 +12,10 @@ class TestSpool(unittest.TestCase):
 
     def test_gmech_init(self):
         """Tests the Init arguments."""
-        self.assertEqual(self.gmech.weekly_pool,300000)
+        self.assertEqual(self.gmech.weekly_pool,500000)
         self.assertEqual(self.gmech.matches_per_hour,3)
-        self.assertEqual(self.gmech.inactivity_time,5)
-        self.assertEqual(self.gmech.inactivity_drop,0.20)
+        self.assertEqual(self.gmech.inactivity_time,10)
+        self.assertEqual(self.gmech.inactivity_drop,0.40)
         self.assertEqual(self.gmech.points_min,1000)
         self.assertEqual(self.gmech.default_matches,100)
         self.assertEqual(self.gmech.weeks_in_history,3)
@@ -29,9 +29,9 @@ class TestSpool(unittest.TestCase):
 
     def test_pool_reduction(self):
         """Tests how point reduction works."""
-        self.assertEqual(self.gmech.get_point_reduction(hours=0, this_weeks_pool=100),20)
-        self.assertEqual(self.gmech.get_point_reduction(hours=2.5, this_weeks_pool=100),10)
-        self.assertEqual(self.gmech.get_point_reduction(hours=5, this_weeks_pool=100),0)
+        self.assertEqual(self.gmech.get_point_reduction(hours=0, this_weeks_pool=100),40)
+        self.assertEqual(self.gmech.get_point_reduction(hours=2.5, this_weeks_pool=100),30)
+        self.assertEqual(self.gmech.get_point_reduction(hours=5, this_weeks_pool=100),20)
         self.assertEqual(self.gmech.get_point_reduction(hours=200, this_weeks_pool=100),0)
 
     def test_match_points_no_history(self):
@@ -43,7 +43,7 @@ class TestSpool(unittest.TestCase):
 
     def test_match_points_with_history(self):
         """Tests the point distribution for merc with a history."""
-        self.assertEqual(self.gmech.get_match_points(0, 10, 500000), 16667)
+        self.assertEqual(self.gmech.get_match_points(0, 10, 500000), 16666)
         self.assertEqual(self.gmech.get_match_points(1, 10, 483333), 16609)
         self.assertEqual(self.gmech.get_match_points(29, 10, 51143), 13114)
         self.assertEqual(self.gmech.get_match_points(30, 10, 38029), 12676)
